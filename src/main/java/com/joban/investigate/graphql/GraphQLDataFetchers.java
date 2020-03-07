c1b5a2bbc7322fd8f098dee4d5619561b9511fe8
蔡俊鹏 <jp.cai@aftership.com>
2020-03-07 11:22:13 +0800

    code style

--- src/main/java/com/joban/investigate/graphql/GraphQLDataFetchers.java
+++ src/main/java/com/joban/investigate/graphql/GraphQLDataFetchers.java
@@ -2,11 +2,9 @@ package com.joban.investigate.graphql;
 
 import com.google.common.collect.ImmutableMap;
 import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -15,80 +13,80 @@ import java.util.stream.Collectors;
 @Component
 public class GraphQLDataFetchers {
 
-  // 初始化一些数据，这些数据可以从数据库里查询出来
-  // 创建books集合
-  private static List<Map<String, String>> books = Arrays.asList(
-      ImmutableMap.of("id", "book-1",
-          "name", "Harry Potter and the Philosopher's Stone",
-          "pageCount", "223",
-          "authorId", "author-1",
-          "categoryId", "category-2"),
-      ImmutableMap.of("id", "book-2",
-          "name", "Moby Dick",
-          "pageCount", "635",
-          "authorId", "author-2",
-          "categoryId", "category-1"),
-      ImmutableMap.of("id", "book-3",
-          "name", "Interview with the vampire",
-          "pageCount", "371",
-          "authorId", "author-3")
-  );
+    // 初始化一些数据，这些数据可以从数据库里查询出来
+    // 创建books集合
+    private static List<Map<String, String>> books = Arrays.asList(
+            ImmutableMap.of("id", "book-1",
+                    "name", "Harry Potter and the Philosopher's Stone",
+                    "pageCount", "223",
+                    "authorId", "author-1",
+                    "categoryId", "category-2"),
+            ImmutableMap.of("id", "book-2",
+                    "name", "Moby Dick",
+                    "pageCount", "635",
+                    "authorId", "author-2",
+                    "categoryId", "category-1"),
+            ImmutableMap.of("id", "book-3",
+                    "name", "Interview with the vampire",
+                    "pageCount", "371",
+                    "authorId", "author-3")
+    );
 
-  // 创建authors集合
-  private static List<Map<String, String>> authors = Arrays.asList(
-      ImmutableMap.of("id", "author-1",
-          "firstName", "Joanne",
-          "lastName", "Rowling"),
-      ImmutableMap.of("id", "author-2",
-          "firstName", "Herman",
-          "lastName", "Melville"),
-      ImmutableMap.of("id", "author-3",
-          "firstName", "Anne",
-          "lastName", "Rice")
-  );
+    // 创建authors集合
+    private static List<Map<String, String>> authors = Arrays.asList(
+            ImmutableMap.of("id", "author-1",
+                    "firstName", "Joanne",
+                    "lastName", "Rowling"),
+            ImmutableMap.of("id", "author-2",
+                    "firstName", "Herman",
+                    "lastName", "Melville"),
+            ImmutableMap.of("id", "author-3",
+                    "firstName", "Anne",
+                    "lastName", "Rice")
+    );
 
-  // 创建categories集合
-  private static List<Map<String, String>> categories = Arrays.asList(
-      ImmutableMap.of("id", "category-1",
-          "name", "Programmer"),
-      ImmutableMap.of("id", "category-2",
-          "name", "Science"),
-      ImmutableMap.of("id", "category-3",
-          "name", "History")
-      );
+    // 创建categories集合
+    private static List<Map<String, String>> categories = Arrays.asList(
+            ImmutableMap.of("id", "category-1",
+                    "name", "Programmer"),
+            ImmutableMap.of("id", "category-2",
+                    "name", "Science"),
+            ImmutableMap.of("id", "category-3",
+                    "name", "History")
+    );
 
-  // 根据bookId获取book对象数据
-  public DataFetcher getBookByIdDataFetcher() {
-    return dataFetchingEnvironment -> {
-      String bookId = dataFetchingEnvironment.getArgument("id");
-      if (StringUtils.isEmpty(bookId)) {
-        return books;
-      }
-      return books
-          .stream()
-          .filter(book -> book.get("id").equals(bookId))
-          .collect(Collectors.toList());
-    };
-  }
+    // 根据bookId获取book对象数据
+    public DataFetcher getBookByIdDataFetcher() {
+        return dataFetchingEnvironment -> {
+            String bookId = dataFetchingEnvironment.getArgument("id");
+            if (StringUtils.isEmpty(bookId)) {
+                return books;
+            }
+            return books
+                    .stream()
+                    .filter(book -> book.get("id").equals(bookId))
+                    .collect(Collectors.toList());
+        };
+    }
 
-  // 从book对象里拿到authorId，然后再获取Author对象数据
-  public DataFetcher getAuthorDataFetcher() {
-    return dataFetchingEnvironment -> {
-      Map<String, String> book = dataFetchingEnvironment.getSource();
-      String authorId = book.get("authorId");
-      return authors
-              .stream()
-              .filter(author -> author.get("id").equals(authorId))
-              .findFirst()
-              .orElse(null);
-    };
-  }
+    // 从book对象里拿到authorId，然后再获取Author对象数据
+    public DataFetcher getAuthorDataFetcher() {
+        return dataFetchingEnvironment -> {
+            Map<String, String> book = dataFetchingEnvironment.getSource();
+            String authorId = book.get("authorId");
+            return authors
+                    .stream()
+                    .filter(author -> author.get("id").equals(authorId))
+                    .findFirst()
+                    .orElse(null);
+        };
+    }
 
-  // 从book对象里拿到categoryId，然后再获取Category对象数据
-  public DataFetcher getCategoryDataFetcher() {
-    return dataFetchingEnvironment -> {
-      String categoryId = dataFetchingEnvironment.getArgument("id");
-      return books.stream().filter(book -> book.get("categoryId").equals(categoryId)).findFirst().orElse(null);
-    };
-  }
+    // 从book对象里拿到categoryId，然后再获取Category对象数据
+    public DataFetcher getCategoryDataFetcher() {
+        return dataFetchingEnvironment -> {
+            String categoryId = dataFetchingEnvironment.getArgument("id");
+            return books.stream().filter(book -> book.get("categoryId").equals(categoryId)).findFirst().orElse(null);
+        };
+    }
 }
--- src/main/java/com/joban/investigate/graphql/GraphQLProvider.java
+++ src/main/java/com/joban/investigate/graphql/GraphQLProvider.java
@@ -3,8 +3,6 @@ package com.joban.investigate.graphql;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import graphql.GraphQL;
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -23,44 +21,44 @@ import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 @Component
 public class GraphQLProvider {
 
-  private GraphQL graphQL;
-  @Autowired
-  GraphQLDataFetchers graphQLDataFetchers;
+    private GraphQL graphQL;
+    @Autowired
+    GraphQLDataFetchers graphQLDataFetchers;
 
-  @Bean
-  public GraphQL graphQL() {
-    return graphQL;
-  }
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
 
-  @PostConstruct
-  public void init() throws IOException {
-    URL url = Resources.getResource("schema.graphqls");
-    String sdl = Resources.toString(url, Charsets.UTF_8);
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        // 构建schema
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
     // 构建schema
-    GraphQLSchema graphQLSchema = buildSchema(sdl);
-    this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-  }
+    // 通过从配置文件里加载的schema来构建GraphQLSchema
+    private GraphQLSchema buildSchema(String sdl) {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
 
-  // 构建schema
-  // 通过从配置文件里加载的schema来构建GraphQLSchema
-  private GraphQLSchema buildSchema(String sdl) {
-    TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
-    RuntimeWiring runtimeWiring = buildWiring();
-    SchemaGenerator schemaGenerator = new SchemaGenerator();
-    return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
-  }
-
-  private RuntimeWiring buildWiring() {
-    return RuntimeWiring.newRuntimeWiring()
-            // 指定Query对象里定义的方法如何实现
-            .type(newTypeWiring("Query")
-                    .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
-            .type(newTypeWiring("Query")
-                    .dataFetcher("categoryById", graphQLDataFetchers.getCategoryDataFetcher()))
-            // 指定Book对象里定义的对象如何查询
-            .type(newTypeWiring("Book")
-                    .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher())
-                    .dataFetcher("category", graphQLDataFetchers.getCategoryDataFetcher()))
-            .build();
-  }
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                // 指定Query对象里定义的方法如何实现
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Query")
+                        .dataFetcher("categoryById", graphQLDataFetchers.getCategoryDataFetcher()))
+                // 指定Book对象里定义的对象如何查询
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher())
+                        .dataFetcher("category", graphQLDataFetchers.getCategoryDataFetcher()))
+                .build();
+    }
 }
